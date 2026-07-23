7d1b1f3e405fe3237db24af6cef4adb544aaf9ed
Janujagruti <ranejanhavi1@gmail.com>
2026-07-23 16:16:18 +0530

    Add string

---
+++ string.py
@@ -0,0 +1,17 @@
+# string.py
+
+text = "Python Programming"
+
+print("Original:", text)
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("Title:", text.title())
+print("Length:", len(text))
+print("Replace:", text.replace("Python", "Java"))
+print("Split:", text.split())
+print("First Character:", text[0])
+
+name = "Rahul"
+age = 21
+
+print(f"My name is {name} and I am {age} years old.")
