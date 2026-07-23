4203d00a604771f563a025548775b9f8760c5d40
Janujagruti <ranejanhavi1@gmail.com>
2026-07-23 16:44:07 +0530

    data types

---
+++ data_types.py
@@ -0,0 +1,30 @@
+# data_types_type_conversion.py
+
+# Different data types in Python
+
+# Integer
+number = 10
+print("Integer:", number)
+print("Type:", type(number))
+
+# Float
+decimal = 10.5
+print("\nFloat:", decimal)
+print("Type:", type(decimal))
+
+# String
+name = "Python"
+print("\nString:", name)
+print("Type:", type(name))
+
+# Boolean
+is_active = True
+print("\nBoolean:", is_active)
+print("Type:", type(is_active))
+
+# Complex number
+complex_number = 3 + 4j
+print("\nComplex:", complex_number)
+print("Type:", type(complex_number))
+
+
