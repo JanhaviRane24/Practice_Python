fb1f533f28adfd26ceba94d7e5a52d34e1c3ca72
Janujagruti <ranejanhavi1@gmail.com>
2026-07-29 19:07:19 +0530

    changes in opps notes

--- Practice Set/chapter_10/notes/oops_concept.py
+++ Practice Set/chapter_10/notes/oops_concept.py
@@ -1,5 +1,75 @@
 # oops_concepts.py
 
+# Object Oriented Programming
+# Solving a problem by creating object is one of the most popular approaches in programming. This is called
+# object-oriented programming.
+# This concept focuses on using reusable code (DRY Principle).
+# Class
+# A class is a blueprint for creating object.
+# Syntax:
+class Eagle: # Class name is written in pascal case
+    pass
+# # Methods & Variables
+
+# Object
+# An object is an instantiation of a class. When class is defined, a template (info) is defined. Memory is
+# allocated only after object instantiation.
+# Objects of a given class can invoke the methods available to it without revealing the implementation details to
+# the user. – Abstractions & Encapsulation!
+
+# Modelling A Problem In OOPS
+# We identify the following in our problem.
+# Noun → Class → Employee
+# Adjective →Attributes →name, age, salary
+# Verbs → Methods →getSalary(), increment()
+
+# Class Attributes
+# An attribute that belongs to the class rather than a particular object.
+# Example:
+
+class Employee:
+    company = "Google" # Specific to Each Class
+
+harry = Employee() # Object Instantiation
+harry.company
+Employee.company = "YouTube" # Changing Class Attribute
+# Instance Attributes
+# An attribute that belongs to the Instance (object). Assuming the class from the previous example:
+harry.name = "harry"
+harry.salary = "30k" # Adding instance attribute
+
+# Note: Instance attributes take preference over class attributes during assignment & retrieval.
+# When looking up for harry.attribute it checks for the following:
+# 1) Is attribute present in object?
+# 2) Is attribute present in class?
+
+# Self Parameter
+# self refers to the instance of the class. It is automatically passed with a function call from an object.
+harry.getSalary() # here self is harry
+# # equivalent to Employee.getSalary(harry)
+# The function getSalary() is defined as:
+class Employee:
+    company = "Google"
+    def getSalary(self):
+        print("Salary is not there")
+
+# Static Method
+# Sometimes we need a function that does not use the self-parameter. We can define a static method like this
+
+@staticmethod  # decorator to mark greet as a static method
+def greet():
+    print("Hello user")
+# __init__() Constructor
+# __init__() is a special method which is first run as soon as the object is created.
+# __init__() method is also known as constructor.
+# It takes ‘selfʼ argument and can also take further arguments.
+# For Example:
+class Employee:
+    def __init__(self, name):
+        self.name=name
+    def getSalary(self):
+        pass
+harry = Employee("Harry")
 
 # =========================
 # Class and Object
